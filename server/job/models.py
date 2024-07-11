5b4e00266bd7cb74461be46178566f8cb96ad36b
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-07-10 22:35:09 -0300

    feat: migrations, admin e model

--- server/job/models.py
+++ server/job/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    modelo_contratacao = models.CharField(max_length=15)
+    senioridade = models.CharField(max_length=20)
+    site_busca = models.CharField(max_length=15)
