22c385072f1d747d83eb49540cb3c306d4a4c37b
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-07-11 20:51:26 -0300

    Fix: Relcionamento entre as tabelas

--- server/job/models.py
+++ server/job/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Portal(models.Model):
+    nome = models.CharField(max_length=20)
+    url = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.nome
+
 class Job(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField()
     modelo_contratacao = models.CharField(max_length=15)
     senioridade = models.CharField(max_length=20)
-    site_busca = models.CharField(max_length=15)
+    site_busca = models.ForeignKey(Portal, on_delete=models.DO_NOTHING)
 
     def __str__(self) -> str:
         return self.titulo
     
-
-class Portal(models.Model):
-    nome = models.CharField(max_length=20)
-    url = models.CharField(max_length=100)
