73e2ab7847f694d4c99a1ea59233bbabc281762b
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-08-29 22:37:21 -0300

    feat: init hunter-api

---
+++ utils/hunter_api.py
@@ -0,0 +1,18 @@
+from requests import Request
+
+class hunterApi:
+
+    def __init__(self):
+        self.base_url = "http://localhost:8000"
+
+    def retorna_listagem_jobs():
+        pass
+
+    def enviar_job():
+        pass
+
+    def atualiza_jobs():
+        pass
+
+    def deleta_job():
+        pass
