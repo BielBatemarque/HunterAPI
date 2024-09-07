b8fd6cf4ebfc8f323c9a54aced79cb2262902f74
BielBaemarque <gabrielb.sdeoliveira12@gmail.com>
2024-09-07 13:43:29 -0300

    fix: criando hunter_api

--- services/hunter_api.py
+++ services/hunter_api.py
@@ -1,10 +1,23 @@
 from models.http_request import HttpRequest
+from models.job import Job
 
-class HunterApi:
+class HunterApi(HttpRequest):
+
 
     def __init__(self):
-        self.base_utl = "http://localhost:8000"
+        super().__init__()
+        self.base_utl = "http://localhost"
+        
+    def enviar_job(self, job: Job) -> str:
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        self.abrir_requisicao(link=self.base_utl, headers=headers, json=job.__dict__)
+        self.enviar_requisicao()
+
+        if self.resposta.status_code >= 200 and self.resposta.status_code < 400:
+            pass
 
         
-    def enviar_job():
-        pass
+
